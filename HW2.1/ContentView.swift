f9dae1001ec38eda2b811e111a6c74e43244698d
Anatoliy <tolxpams@gmail.com>
2021-03-21 13:39:53 +0200

    Initial Commit

---
+++ HW2.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW2.1
+//
+//  Created by Anatoliy Khramchenko on 21.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW2.1/HW2_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW2_1App.swift
+//  HW2.1
+//
+//  Created by Anatoliy Khramchenko on 21.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW2_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
